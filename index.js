c8db91f670e8dafd8ffba914e13437a9e0236175
Kateryna Soloviova <kateryna.soloviova@gmail.com>
2025-07-07 16:19:19 +0200

    Solved task from lan intro node

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) {
+    let i = 0;
+    while(i < this.items.length && this.items[i] < item){
+      i++;
+    }
 
-  get(pos) {}
+    this.items.splice(i, 0, item);
 
-  max() {}
+    this.length = this.items.length;
+  }
 
-  min() {}
+  get(pos) {
+    if (pos < 0 || pos >= this.length) {
+      throw new Error("OutOfBounds");
+    }
+    return this.items[pos];
+  }
 
-  sum() {}
 
-  avg() {}
+
+  max() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+
+    return Math.max(...this.items)
+  }
+
+  min() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return Math.min(...this.items)
+  }
+
+  sum() {
+    if(this.length === 0){
+      return 0;
+    }
+    return this.items.reduce((acc, item)=> acc + item, 0);
+  }
+
+  avg() {
+    if (this.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return this.sum() / this.length;
+  }
 }
 
 module.exports = SortedList;
